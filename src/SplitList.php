a6f33a96e1ecc2ef7b6884862dc497e422e82dc1
Eugene <goncharovei@gmail.com>
2020-11-06 16:38:27 +0200

    added PHPUnit

---
+++ src/SplitList.php
@@ -0,0 +1,6 @@
+<?php
+
+function splitList($list, $n)
+{
+    return true;
+}
---
+++ tests/Unit/SplitListTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SplitListTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(splitList(1,1));
+    }
+}
